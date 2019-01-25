500c5b7217714c5e779166da4c27ab99579d3220
Trai Lynne <trailynne@gmail.com>
2019-01-25 13:16:51 -0500

    Actions: Defined POST request handler for CREATE operation used async/await checks for existing project success => creates post successfully incomplete => 400 with error message internal error => 500 with error message

--- routers/actionRoutes.js
+++ routers/actionRoutes.js
@@ -1,17 +1,95 @@
 const express = require('express');
 
 const router = express.Router();
+const db = require('../data/helpers/actionModel');
+const projectDb = require('../data/helpers/projectModel');
 
 router.use(express.json());
 
 // C - Create
-router.post('/', (req, res) => {
-    res
-        .status(201)
-        .json({
-            url: '/api/actions/',
-            operation: 'POST'
-        });
+router.post('/', async (req, res) => {
+    let projectRecord = null;
+
+    const newRecord = req.body;
+
+    try {
+        if (!newRecord.description || newRecord.description === '') {
+            res
+                .status(400)
+                .json({
+                    errorMessage: 'Please provide a description for this new record'
+                });
+        } else if (!newRecord.notes || newRecord.notes === '') {
+            res
+                .status(400)
+                .json({
+                    errorMessage: 'Please provide notes for this new record'
+                });
+        } else if (!newRecord.project_id) {
+            res
+                .status(400)
+                .json({
+                    errorMessage: 'Please provide an existing project this action will be attached to.'
+                });
+        } else if (!newRecord.completed) {
+            projectRecord = await projectDb.findById(newRecord.project_id);
+
+            if (!projectRecord) {
+                res
+                    .status(400)
+                    .json({
+                        errorMessage: 'Please provide an existing project this action will be attached to.'
+                    });
+            } else {
+                newRecord.completed = false;
+
+                const newId = await db.create(newRecord);
+
+                newId ?
+                    res
+                        .status(200)
+                        .json({
+                            newId: newId[0]
+                        })
+                    :
+                    res
+                        .status(500)
+                        .json({
+                            errorMessage: 'There was an error processing a request. Please try again'
+                        });
+            }
+        } else {
+            projectRecord = await projectDb.findById(newRecord.project_id);
+
+            if (!projectRecord) {
+                res
+                    .status(400)
+                    .json({
+                        errorMessage: 'Please provide an existing project this action will be attached to.'
+                    });
+            } else {
+                const newId = await db.create(newRecord);
+
+                newId ?
+                    res
+                    .status(200)
+                    .json({
+                        newId: newId[0]
+                    }) :
+                    res
+                    .status(500)
+                    .json({
+                        errorMessage: 'There was an error processing a request. Please try again'
+                    });
+            }
+        }
+    } catch (err) {
+        res
+            .status(500)
+            .json({
+                errorMessage: 'Houston, we have a problem.'
+            });
+    }
 });
 
 // R - Read
