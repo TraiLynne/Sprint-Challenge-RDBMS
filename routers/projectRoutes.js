e5038f5e6ee8278505ae855490d175e43bb6015e
Trai Lynne <trailynne@gmail.com>
2019-01-25 12:53:01 -0500

    Projects: Defined POST request handler for CREATE operation uses async/await success => 201 with new id incomplete => 400 with error message internal error => 500 wih error message

--- routers/projectRoutes.js
+++ routers/projectRoutes.js
@@ -1,17 +1,65 @@
 const express = require('express');
 
 const router = express.Router();
+const db = require('../data/helpers/projectModel');
 
 router.use(express.json());
 
 // C - Create
-router.post('/', (req, res) => {
-    res
-        .status(201)
-        .json({
-            url: '/api/projects/',
-            operation: 'POST'
-        });
+router.post('/', async (req, res) => {
+    const newRecord = req.body;
+    try {
+        if (!newRecord.name || !newRecord.name === '') {
+            res
+                .status(400)
+                .json({
+                    errorMessage: 'Please provide a name for the new record'
+                });
+        } else if (!newRecord.description || newRecord.description === '') {
+            res
+                .status(400)
+                .json({
+                    errorMessage: 'Please provide a description for the new record'
+                });
+        } else if (!newRecord.completed) {
+            newRecord.completed = false;
+
+            const newId = await db.create(newRecord);
+
+            newId ?
+                res
+                    .status(201)
+                    .json({
+                        newId: newId[0]
+                    })
+            :
+            res
+                .status(500)
+                .json({
+                    errorMessage: 'There was an error processing your request. Please try again'
+                });
+        } else {
+            const newId = await db.create(newRecord);
+
+            newId ?
+                res
+                .status(201)
+                .json({
+                    newId: newId[0]
+                }) :
+                res
+                .status(500)
+                .json({
+                    errorMessage: 'There was an error processing your request. Please try again'
+                });
+        }
+    } catch (err) {
+        res
+            .status(500)
+            .json({
+                errorMessage: 'Houston, we have a problem'
+            });
+    }
 });
 
 // R - Read
