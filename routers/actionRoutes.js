d478d5f40240022bd0c4053afa66057f3edf371d
Trai Lynne <trailynne@gmail.com>
2019-02-22 13:28:58 -0500

    Define Sub-Router Boilerplates

--- routers/actionRoutes.js
+++ routers/actionRoutes.js
@@ -5,6 +5,54 @@ const router = express.Router();
 router.use(express.json());
 
 // Routes
+// C - Create
+router.post('/', (req, res) => {
+    res
+        .status(201)
+        .json({
+            operation: 'POST',
+            url: '/api/actions/'
+        });
+});
+
+// R - Read
+router.get('/', (req, res) => {
+    res
+        .status(200)
+        .json({
+            operation: 'GET',
+            url: '/api/actions/'
+        });
+});
+
+router.get('/:id', (req, res) => {
+    res
+        .status(200)
+        .json({
+            operation: 'GET',
+            url: '/api/actions/:id'
+        });
+});
+
+// U - Update
+router.put('/:id', (req, res) => {
+    res
+        .status(200)
+        .json({
+            operation: 'PUT',
+            url: '/api/actions/:id'
+        });
+});
+
+// D - Destroy
+router.delete('/:id', (req, res) => {
+    res
+        .status(200)
+        .json({
+            operation: 'DELETE',
+            url: '/api/actions/:id'
+        });
+});
 
 router.use('/', (req, res) => res.send('Welcome to the Action API'));
 
--- routers/projectRoutes.js
+++ routers/projectRoutes.js
@@ -5,6 +5,63 @@ const router = express.Router();
 router.use(express.json());
 
 // Routes
+// C - Create
+router.post('/', (req, res) => {
+    res
+        .status(201)
+        .json({
+            operation: 'POST',
+            url: '/api/projects/'
+        });
+});
+
+// R - Read
+router.get('/', (req, res) => {
+    res
+        .status(200)
+        .json({
+            operation: 'GET',
+            url: '/api/projects/'
+        });
+});
+
+router.get('/:id', (req, res) => {
+    res
+        .status(200)
+        .json({
+            operation: 'GET',
+            url: '/api/projects/:id'
+        });
+});
+
+router.get('/:id/actions', (req, res) => {
+    res
+        .status(200)
+        .json({
+            operation: 'GET',
+            url: '/api/projects/:id/actions'
+        });
+});
+
+// U - Update
+router.put('/:id', (req, res) => {
+    res
+        .status(200)
+        .json({
+            operation: 'PUT',
+            url: '/api/projects/:id'
+        });
+});
+
+// D - Destroy
+router.delete('/:id', (req, res) => {
+    res
+        .status(200)
+        .json({
+            operation: 'DELETE',
+            url: '/api/projects/:id'
+        });
+});
 
 router.use('/', (req, res) => res.send('Welcome to the Project API'));
 
