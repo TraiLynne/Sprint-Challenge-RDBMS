7224bb9fbabbd401c01998284f75689b1e093d88
Trai Lynne <trailynne@gmail.com>
2019-02-22 13:14:23 -0500

    Define DB Helpers Model defined for each DB table

--- db/helpers/actionModel.js
+++ db/helpers/actionModel.js
@@ -5,14 +5,31 @@ const db = knex(dbConfig.development);
 const dbName = 'actions';
 
 // C - Create
+const createAction = newAction => db(dbName).insert(newAction);
 
 // R - Read
+const readActions = () => db(dbName);
+
+const readAction = id => db(dbName).where({
+    id
+}).first();
+
 
 // U - Update
+const updateAction = (id, updatedAction) => db(dbName).where({
+    id
+}).update(updatedAction);
 
 // D - Destory
+const destroyAction = id => db(dbName).where({
+    id
+}).del();
 
 
 module.exports = {
-
+    createAction,
+    readActions,
+    readAction,
+    updateAction,
+    destroyAction
 }
--- db/helpers/projectModel.js
+++ db/helpers/projectModel.js
@@ -5,14 +5,30 @@ const db = knex(dbConfig.development);
 const dbName = 'projects';
 
 // C - Create
+const createProject = newProject => db(dbName).insert(newProject);
 
 // R - Read
+const readProjects = () => db(dbName);
+
+const readProject = id => db(dbName).where({ id }).first();
+
+const readProjectActions = id => db('actions').where({ project_id: id});
 
 // U - Update
+const updateProject = (id, updatedProject) => db(dbName).where({ id }).update(updatedProject);
 
 // D - Destory
+const destroyProject = id => db(dbName).where({id}).del();
+
+const destroyProjectActions = id => db('actions').where({ project_id: id}).del();
 
 
 module.exports = {
-    
+    createProject,
+    readProjects,
+    readProject,
+    readProjectActions,
+    updateProject,
+    destroyProject,
+    destroyProjectActions
 }
